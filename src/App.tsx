278dac298b0492dc3d94c9da9feb7b07e58c086c
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2021-01-26 13:12:39 +0100

    Add button that produces an error on click

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import * as Sentry from '@sentry/react';
 import React from 'react';
 import { ExampleGrid } from './components/ExampleGrid';
 
+const onClick = () => {
+  throw new Error('foo');
+}
+
 function App() {
   return (
     <main>
       <ExampleGrid/>
+      <button onClick={onClick}>Produce Error</button>
     </main>
   );
 }
