7a4b4c0970609a66ff012feb628beab6801f1f4a
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2021-01-26 12:12:39 +0100

    Add example grid

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ExampleGrid } from './components/ExampleGrid';
+
+function App() {
+  return (
+    <main>
+      <ExampleGrid />
+    </main>
+  );
+}
+
+export default App;
